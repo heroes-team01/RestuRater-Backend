1092400bf05c666a7e5a95c2ee156b5174628576
Heba1998 <hebaalmomani1998@gmail.com>
2021-08-15 10:53:02 +0300

    first part

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 const cors = require('cors');
-
 app.use(cors()) // after you initialize your express app instance
-require('dotenv').config();
-const axios = require('axios'); // require the package
+require('dotenv').config();const PORT = process.env.PORT;
+app.use(express.json());
+const MONGODB_CLINTE = process.env.MONGODB_CLINTE;
+const mongoose = require("mongoose");
+// const { seedUserData } = require("./models/users.model");
 
-// inside your callback function
-axios.get(url).then(response => response.data).catch(error => console.log(error));
+// const { addUser, getPage } = require("./controller/users.controller");
 
-// a server endpoint 
-app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-})
- 
-app.listen(3000) // kick start the express server to work
+
+// const {
+//   createComment,
+//   updateComment,
+//   deleteComment,
+// } = require("./controller/comments.controller");
+
+// seedUserData();
+//////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////
+// app.post("/user", addUser);
+
+// app.post("/comment/:recipes_id", createComment);
+// app.put("/comment/:comment_id", updateComment);
+// app.delete("/comment/:comment_id", deleteComment);
+/////////////////////////////////////////////////////////////////
+
+
+
+app.get("/", function (req, res) {
+  res.send("Welcome to the server of Heros Team");
+});
+
+app.listen(PORT);
